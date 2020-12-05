89e6ee8cfc895b85953e2ba60ae794223f48903b
Fewing <yangzheyu98@gmail.com>
2020-12-06 00:04:59 +0800

    基本功能完成

--- flightaware/items.py
+++ flightaware/items.py
@@ -9,4 +9,11 @@ import scrapy
 class FlightawareItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    data_type = scrapy.Field()
+    f_ident = scrapy.Field()
+    f_airline = scrapy.Field()
+    f_type = scrapy.Field()
+    f_from = scrapy.Field()
+    f_to = scrapy.Field()
+    f_depart = scrapy.Field()
+    f_arrive = scrapy.Field()
--- flightaware/pipelines.py
+++ flightaware/pipelines.py
@@ -5,9 +5,37 @@
 
 
 # useful for handling different item types with a single interface
+import json
 from itemadapter import ItemAdapter
+from scrapy.exporters import JsonItemExporter
 
 
 class FlightawarePipeline:
+
+    arrive = []
+    departure = []
+    enroute = []
+    scheduled = []
+
+    def open_spider(self, spider):
+        self.file = open('kjfk.json', 'wb')
+
+    def close_spider(self, spider):
+        output_json = {}
+        output_json['arrive'] = self.arrive
+        output_json['departure'] = self.departure
+        output_json['enroute'] = self.enroute
+        output_json['scheduled'] = self.scheduled
+        self.file.write(json.dumps(output_json,ensure_ascii=False).encode('utf-8')) #输出到文件
+        self.file.close()
+
     def process_item(self, item, spider):
+        if item['data_type'] == 'arrive':
+            self.arrive.append(dict(item))
+        elif item['data_type'] == 'departure':
+            self.departure.append(dict(item))
+        elif item['data_type'] == 'enroute':
+            self.enroute.append(dict(item))
+        elif item['data_type'] == 'scheduled':
+            self.scheduled.append(dict(item)) #暂存到列表
         return item
